4054bfaafdc6532d6844e7bd414698d6391c7512
Benjamin Preiss <benjaminpreiss@protonmail.com>
2021-12-08 14:58:30 +0100

    Update database.js
    Update database to fix "Knex: Timeout acquiring a connection. The pool is probably full." error on deployment

--- config/database.js
+++ config/database.js
@@ -11,6 +11,13 @@ module.exports = ({ env }) => ({
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
     },
-    debug: false,
+    debug: true,
   },
+  pool: {
+    "min": 0,
+    "max": 10,
+    "idleTimeoutMillis": 30000,
+    "createTimeoutMillis": 30000,
+    "acquireTimeoutMillis": 30000
+  }
 });
