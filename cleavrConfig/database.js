480d2adf20e6d0b99958d16661551e6b5b432da4
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-01-28 10:51:17 +0100

    Change postgres to mysql option

--- cleavrConfig/database.js
+++ cleavrConfig/database.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
+      port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
@@ -11,13 +11,6 @@ module.exports = ({ env }) => ({
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
     },
-    debug: true,
-    pool: {
-      "min": 0,
-      "max": 10,
-      "idleTimeoutMillis": 30000,
-      "createTimeoutMillis": 30000,
-      "acquireTimeoutMillis": 30000
-    }
-  }
+    debug: false,
+  },
 });
